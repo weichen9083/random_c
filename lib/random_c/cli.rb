39b69660e2304860841a411b4c38d8bbfa8973ec
weichen9083 <weichen9083@gmail.com>
2021-02-16 23:46:30 -0500

    update 1

--- lib/random_c/cli.rb
+++ lib/random_c/cli.rb
@@ -1,8 +1,18 @@
+
 class RandomC::CLI 
 
     def call
         puts "Wecolme to randomc"
-        binding.pry 
-    end 
+        # show_list_of_anime 
+        # ask_user_which_one_to_view
+        # show_detail_on_the_anime
+     end
+     
+     def get_anime_list
+         
+     end 
+     def ask_user_for_anime 
+        
+     end 
 
 end 
--- notes.md
+++ notes.md
@@ -5,3 +5,10 @@
     - users see the list of anime preview for the season
     - users select the anime 
     - user see the anime details 
+
+
+
+Classes 
+-anime 
+-CLI
+-Scraper 
